748a02bd9604351458d71767ff3bcd697213b4f9
seppowalther <seppo.walther@educorvi.de>
2019-11-28 09:54:22 +0100

    Working on schutzhandschuh.py

--- src/bgetem/hautschutz/content/schutzhandschuh.py
+++ src/bgetem/hautschutz/content/schutzhandschuh.py
@@ -13,47 +13,47 @@ from zope.interface import implementer
 
 
 class ISchutzhandschuh(model.Schema):
-    """ Marker interface and Dexterity Python Schema for Schutzhandschuh
+    """ 
+    Schema eines Schutzhandschuhs
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('schutzhandschuh.xml')
+    title = schema.TextLine(title=u"Produktname")
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    text = RichText(title=u"Weitere Beschreibungen des Produkts", required = False)
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    bild = NamedBlobImage(title=u"Produktbild", required=False)
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+    hersteller = RelationChoice(
+            title=u"Hersteller oder Lieferant",
+            source=ObjPathSourceBinder(),
+            required=False,
+            )
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
+    form.widget(material_aussen=CheckBoxFieldWidget)
+    material_aussen = schema.List(title=u"Material außen", value_type=schema.Choice(vocabulary=material), required=False,)
 
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
+    form.widget(material_innen=CheckBoxFieldWidget)
+    material_innen = schema.List(title=u"Material innen", value_type=schema.Choice(vocabulary=material), required=False)
 
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    form.widget(profilierung=CheckBoxFieldWidget)
+    profilierung = schema.List(tiel=u"Profilierung", value_type=schema.Choice(vocabulary=profilierung), required=False)
+
+    schichtstaerke_min = schema.Float(title=u"Schichtstärke (min) in mm", required=False)
+    schichtstaerke_max = schema.Float(title=u"Schichtstärke (max) in mm", required=False)
+
+    gesamtlaenge_von = schema.Int(title=u"Gesamtlänge (von) in mm", required=False)
+    gesamtlaenge_bis = schema.Int(title=u"Gesamtlänge (bis) in mm", requierd=False)
+
+    cecategory = schema.Choice(title=u"CE-Kategorie", vocabulary=cecatcalues)
+
+    allergene = schema.List(title=u"Allergene", value_type=schema.Choice(vocabulary=allergene_vocab), required=False)
+
+    m1.fieldset(
+            'chemie',
+            label=u"Chemische/Biologische Risiken",
+            fields=['norm374_2003', 'norm374_2016', 'chemikalienliste', 'norm374_5', 'gefahrstoffschutz']
+    )
+
 
 
 @implementer(ISchutzhandschuh)
