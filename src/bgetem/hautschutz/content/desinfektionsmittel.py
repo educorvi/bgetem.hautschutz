a4bd4c1a850c23ff69ef09a0be3ae346a11f26c1
seppowalther <seppo.walther@educorvi.de>
2019-11-29 13:07:56 +0100

    Added import NamedBlobImage

--- src/bgetem/hautschutz/content/desinfektionsmittel.py
+++ src/bgetem/hautschutz/content/desinfektionsmittel.py
@@ -4,6 +4,7 @@ from plone.supermodel import model
 from zope import schema
 from zope.interface import implementer
 from bgetem.hautschutz.vocabularies import desinf_anwendung, desinf_produktgruppe, desinf_wirksamkeit, einwirkzeit, desinf_pruefung
+from plone.namedfile.field import NamedBlobImage
 
 from bgetem.hautschutz import _
 
@@ -49,6 +50,7 @@ class IDesinfektionsmittel(model.Schema):
 
     bemerkungen = schema.Text(title=_(u"Bemerkungen"), required=False)
 
+    bild = NamedBlobImage(title=u"Produktbild", required=False)
 
 #    hersteller = RelationChoice(
 #            title=u"Hersteller oder Lieferant",
