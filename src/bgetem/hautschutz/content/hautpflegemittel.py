4ec5394f1e4c2102784897898a1ab4211e4a885d
seppowalther <seppo.walther@educorvi.de>
2019-12-02 09:04:26 +0100

    Added bild (NamedBlobImage) to hautpflegemittel.py

--- src/bgetem/hautschutz/content/hautpflegemittel.py
+++ src/bgetem/hautschutz/content/hautpflegemittel.py
@@ -8,6 +8,7 @@ from plone.supermodel import model
 # from z3c.form.browser.radio import RadioFieldWidget
 from zope import schema
 from zope.interface import implementer
+from plone.namedfile.field import NamedBlobImage
 from bgetem.hautschutz.vocabularies import anwendungVocabulary
 
 from bgetem.hautschutz import _
@@ -29,7 +30,7 @@ class IHautpflegemittel(model.Schema):
 
     bemerkungen = schema.Text(title=u"Bemerkungen", required=False)
 
-#    bild = NamedBlobImage(title=u"Produktbild", required=False)
+    bild = NamedBlobImage(title=u"Produktbild", required=False)
 
 #    hersteller = RelationChoice(
 #        title=u"Hersteller oder Lieferant",
