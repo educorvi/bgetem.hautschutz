bfa5c5c814bbc0f2db9e5d2fceb7e97d8715b414
pascalpaul <pascalpaul@educorvi.de>
2020-02-05 12:01:38 +0100

    "Add view: hautschutzmittel-view"

--- src/bgetem/hautschutz/views/hautschutzmittel_view.py
+++ src/bgetem/hautschutz/views/hautschutzmittel_view.py
@@ -6,15 +6,12 @@ from Products.Five.browser import BrowserView
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
-class DesinfektionsmittelView(BrowserView):
+class HautschutzmittelView(BrowserView):
     # If you want to define a template here, please remove the template from
     # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('desinfektionsmittel_view.pt')
+    # template = ViewPageTemplateFile('hautschutzmittel_view.pt')
 
     def __call__(self):
         # Implement your own actions:
         self.msg = _(u'A small message')
         return self.index()
-
-    def get_herstellerdaten(self):
-        return u'Hallo Welt'        
